f402ebf8ea6f04a163d923693a0c518d0415335f
a-stanisz <hello@adamstaniszewski.com>
2023-01-08 20:07:39 +0100

    Add logger message

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  logger.log(`App listens at port ${PORT}`);
 }
 bootstrap();
